583df3c42beddce2ce090b12c05e76800477ea3d
Mike Eng <mrengy@gmail.com>
2016-11-26 20:36:06 -0500

    making viewport follow player

--- boilerplate-master/js/entities/entities.js
+++ boilerplate-master/js/entities/entities.js
@@ -9,6 +9,24 @@ game.PlayerEntity = me.Entity.extend({
     init:function (x, y, settings) {
         // call the constructor
         this._super(me.Entity, 'init', [x, y , settings]);
+
+        // set the default horizontal & vertical speed (accel vector)
+        this.body.setVelocity(3,15);
+
+        // set the display to follow our position on both axes
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+
+        // ensure the player is updated even when outside of the viewport
+        this.alwaysUpdate = true;
+
+        // define a basic walking animation (using all frames)
+        this.renderable.addAnimation("walk", [0, 1, 2, 3, 4, 5, 6,7]);
+
+        // define a standing animation (using the first frame)
+        this.renderable.addAnimation("stand", [0]);
+
+        //set the standing animation as default
+        this.renderable.setCurrentAnimation("stand");
     },
 
     /**
